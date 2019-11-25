2a3751b92a2db34794e8723f8829800d98f1bd85
William Campbell <William.Campbell@centeva.com>
2019-11-25 04:17:14 -0600

    Added React UI

---
+++ FunctionBFApp/brushfire-ui/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Main from './Main';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+          <p>
+              My Camping & Outdoor Store
+          </p>
+      </header>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
